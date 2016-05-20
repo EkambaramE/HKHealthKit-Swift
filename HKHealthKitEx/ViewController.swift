76ea0cc4703e46e60116fba9b5dd9effe2615201
Ekambaram E <eekambaram90@gamil.com>
2016-05-20 13:04:34 +0530

    first commit

--- HKHealthKitEx/ViewController.swift
+++ HKHealthKitEx/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
-
+    
+    var healthKit = HKHealthStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func requestForReadAndShareButton(sender: AnyObject) {
+        
+        let healthKitTypesToRead = Set(
+            arrayLiteral: HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth)!,
+            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex)!,
+            HKObjectType.workoutType()
+        )
+        
+        let healthKitTypesToWrite = Set(arrayLiteral:
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex)!,
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned)!,
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)!,
+            HKQuantityType.workoutType()
+            )
+        
+        let newCompletion: ((Bool, NSError?) -> Void) = {
+            (success, error) -> Void in
+            
+            if !success {
+                print("You didn't allow HealthKit to access these write data types.\nThe error was:\n \(error!.description).")
+                
+                return
+            }
+        }
+        
+        healthKit.requestAuthorizationToShareTypes(healthKitTypesToWrite, readTypes: healthKitTypesToRead, completion: newCompletion)
+    }
 
 }
 
